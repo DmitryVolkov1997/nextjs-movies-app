9b8df98b71015371db89fc1914160ef79bae7f22
Dmitriy <dmitrij_volkov_2014@mail.ru>
2022-07-20 16:30:48 +0600

    added https

--- app/components/screens/home/Home.jsx
+++ app/components/screens/home/Home.jsx
@@ -14,27 +14,31 @@ import { setRadio } from '../../../store/radioSlice/radioSlice'
 
 const Home = () => {
 	const dispatch = useDispatch()
-	const { movies = [], loading } = useSelector(state => state.movies)
-	const { radio } = useSelector(state => state.radio)
-	const { search } = useSelector(state => state.search)
-	
+	const { movies = [], loading } = useSelector((state) => state.movies)
+	const { radio } = useSelector((state) => state.radio)
+	const { search } = useSelector((state) => state.search)
+
 	const handleSearch = (nameMovie) => {
 		dispatch(setSearch({ text: nameMovie }))
 	}
-	
+
 	const filterMoviesByName = (search, radio = 'all') => {
 		const fetchData = async () => {
-			const { data } = await axios.get(`${BASE_URL}=${search.length ? search : 'matrix'}${radio !== 'all' ? `&type=${radio}` : ''}`)
+			const { data } = await axios.get(
+				`${BASE_URL}=${search.length ? search : 'matrix'}${
+					radio !== 'all' ? `&type=${radio}` : ''
+				}`
+			)
 			dispatch(setMovies({ data: data.Search }))
 			dispatch(setLoading(false))
 		}
 		fetchData()
 	}
-	
+
 	const handleCategory = (e) => {
 		dispatch(setRadio({ type: e.target.dataset.type }))
 	}
-	
+
 	useEffect(() => {
 		const fetchData = async () => {
 			const { data } = await axios.get(`${BASE_URL}=matrix`)
@@ -43,27 +47,41 @@ const Home = () => {
 		}
 		fetchData()
 	}, [])
-	
+
 	useEffect(() => {
 		filterMoviesByName(search, radio)
 	}, [search, radio])
-	
+
 	return (
 		<div className={classes.home}>
 			<Container>
 				<div className={classes.content}>
 					<Search handleSearch={handleSearch} search={search} />
 					<div className={classes.group}>
-						<Radio title={'All'} name={'category'} checked={radio === 'all'} value={'all'} typeMovie={'all'}
-						       onChange={handleCategory} />
-						<Radio title={'Movies only'} name={'category'} checked={radio === 'movie'} typeMovie={'movie'}
-						       onChange={handleCategory} />
-						<Radio title={'Series only'} name={'category'} checked={radio === 'series'} typeMovie={'series'}
-						       onChange={handleCategory} />
+						<Radio
+							title={'All'}
+							name={'category'}
+							checked={radio === 'all'}
+							value={'all'}
+							typeMovie={'all'}
+							onChange={handleCategory}
+						/>
+						<Radio
+							title={'Movies only'}
+							name={'category'}
+							checked={radio === 'movie'}
+							typeMovie={'movie'}
+							onChange={handleCategory}
+						/>
+						<Radio
+							title={'Series only'}
+							name={'category'}
+							checked={radio === 'series'}
+							typeMovie={'series'}
+							onChange={handleCategory}
+						/>
 					</div>
-					{
-						loading ? <Preloader /> : <MoviesList movies={movies} />
-					}
+					{loading ? <Preloader /> : <MoviesList movies={movies} />}
 				</div>
 			</Container>
 		</div>
--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=6633a9ca&s'
+const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=6633a9ca&s'
 
 const searchByName = (name) => `${BASE_URL}=${name}`
 
-export {BASE_URL, searchByName}
+export { BASE_URL, searchByName }
